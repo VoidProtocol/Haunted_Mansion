3df456286ef845c04d32c4a9dbee9a1a9942a7c5
VoidProtocol <mw.orchowski@gmail.com>
2019-10-28 12:33:32 +0100

    HM-01: Player movement and template graphics added

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Controls:")]
+    [SerializeField]
+    float playerSpeed = 1;
+
+    void Update()
+    {
+        PlayerMovement(playerSpeed);
+    }
+
+    private void PlayerMovement(float playerSpeed)
+    {
+        Vector3 horizontalMovement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        Vector3 verticalMovement = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
+
+        transform.position = transform.position + horizontalMovement * playerSpeed * Time.deltaTime;
+        transform.position = transform.position + verticalMovement * playerSpeed *Time.deltaTime;
+    }
+
+}
